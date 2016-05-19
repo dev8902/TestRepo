8acf7f2f50c2875d372da516683843841c22949c
Naresh <naresh>
2016-05-19 22:11:20 +0530

    file formates

--- src/com/naresh/room/EmployeeTest.java
+++ src/com/naresh/room/EmployeeTest.java
@@ -5,24 +5,25 @@ package com.naresh.room;
 
 /**
  * @author Naresh
- *
+ * 
  */
 public class EmployeeTest {
 
 	/**
 	 * @param args
 	 */
-	static{
+	static {
 		System.out.println("Hello static method....");
-		String[] hello={"one","two","three"};
+		String[] hello = { "one", "two", "three" };
 		main(hello);
 	}
+
 	public static void main(String[] args) {
 		Employee e = new Employee();
 		e.setEmployeeId(100);
 		e.setEmployeeName("naresh");
-		System.out.println("employee id:"+e.getEmployeeId()+"  employeeName:"+e.getEmployeeName());
-		
+		System.out.println("employee id:" + e.getEmployeeId()
+				+ "  employeeName:" + e.getEmployeeName());
 
 	}
 
