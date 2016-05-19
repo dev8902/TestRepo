a36ab9cac83ae77afdb07214ee21e994145af73e
Naresh <naresh>
2016-05-19 20:26:52 +0530

    Employee file added

---
+++ src/com/naresh/room/Employee.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.naresh.room;
+
+/**
+ * @author Naresh
+ *
+ */
+public class Employee {
+	private int employeeId;
+	private String employeeName;
+	/**
+	 * @return the employeeId
+	 */
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	/**
+	 * @param employeeId the employeeId to set
+	 */
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	/**
+	 * @return the employeeName
+	 */
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	/**
+	 * @param employeeName the employeeName to set
+	 */
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+
+}
---
+++ src/com/naresh/room/EmployeeTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.naresh.room;
+
+/**
+ * @author Naresh
+ *
+ */
+public class EmployeeTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
