935457fca149360ec93f51a91b332f1f4ad78c64
Naresh <naresh>
2016-05-19 21:59:25 +0530

    static block has been added

--- src/com/naresh/room/EmployeeTest.java
+++ src/com/naresh/room/EmployeeTest.java
@@ -12,11 +12,15 @@ public class EmployeeTest {
 	/**
 	 * @param args
 	 */
+	static{
+		System.out.println("Hello static method....");
+	}
 	public static void main(String[] args) {
 		Employee e = new Employee();
 		e.setEmployeeId(100);
 		e.setEmployeeName("naresh");
 		System.out.println("employee id:"+e.getEmployeeId()+"  employeeName:"+e.getEmployeeName());
+		
 
 	}
 
