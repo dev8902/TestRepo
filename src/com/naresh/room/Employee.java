45680b2e9b1d335acb42c067e454fde020c5018f
Naresh <naresh>
2016-05-20 07:52:13 +0530

    compable messages added

--- src/com/naresh/room/Employee.java
+++ src/com/naresh/room/Employee.java
@@ -5,35 +5,57 @@ package com.naresh.room;
 
 /**
  * @author Naresh
- *
+ * 
  */
-public class Employee {
+public class Employee implements Comparable<Employee> {
+	public Employee(int employeeId, String employeeName) {
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		// TODO Auto-generated constructor stub
+	}
+
 	private int employeeId;
 	private String employeeName;
+
 	/**
 	 * @return the employeeId
 	 */
 	public int getEmployeeId() {
 		return employeeId;
 	}
+
 	/**
-	 * @param employeeId the employeeId to set
+	 * @param employeeId
+	 *            the employeeId to set
 	 */
 	public void setEmployeeId(int employeeId) {
 		this.employeeId = employeeId;
 	}
+
 	/**
 	 * @return the employeeName
 	 */
 	public String getEmployeeName() {
 		return employeeName;
 	}
+
 	/**
-	 * @param employeeName the employeeName to set
+	 * @param employeeName
+	 *            the employeeName to set
 	 */
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
-	
+
+	@Override
+	public int compareTo(Employee o) {
+		if (this.employeeId > o.employeeId) {
+			return 1;
+		} else if (this.employeeId == o.employeeId) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
 
 }
--- src/com/naresh/room/EmployeeTest.java
+++ src/com/naresh/room/EmployeeTest.java
@@ -3,6 +3,10 @@
  */
 package com.naresh.room;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author Naresh
  * 
@@ -12,24 +16,28 @@ public class EmployeeTest {
 	/**
 	 * @param args
 	 */
-	static {
-		System.out.println("Hello static method....");
-		String[] hello = { "one", "two", "three" };
-		main(hello);
-	}
-	public static String getMessage(){
+
+	public static String getMessage() {
 		return "Hello";
 	}
 
 	public static void main(String[] args) {
-		Employee e = new Employee();
-		e.setEmployeeId(100);
-		e.setEmployeeName("naresh");
-		System.out.println(getMessage()+" "+e.getEmployeeName());
-		System.out.println("employee id:" + e.getEmployeeId()
-				+ "  employeeName:" + e.getEmployeeName());
-		System.out.println("employee id:" + e.getEmployeeId()
-				+ "  employeeName:" + e.getEmployeeName());
+		Employee e = new Employee(105, "naresh");
+		Employee e1 = new Employee(104, "ramesh");
+		Employee e2 = new Employee(103, "rajesh");
+		Employee e3 = new Employee(101, "raghu");
+		List<Employee> list = new ArrayList<Employee>();
+		list.add(e);
+		list.add(e1);
+		list.add(e2);
+		list.add(e3);
+		Collections.sort(list);
+		for (Employee emp : list) {
+			System.out.println("Hello: Mr." + emp.getEmployeeName());
+			System.out.println("Id:" + emp.getEmployeeId() + "  Name:"
+					+ emp.getEmployeeName());
+			System.out.println("==========================");
+		}
 
 	}
 
